b94349b9a63d592267bf3ce0396f640c3a23bb49
akshay896 <akshaydileesh896@gmail.com>
2024-08-09 20:54:34 +0530

    seven-up

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  
+  return (
+    <>
+      <div className="banner">
+        <div className="product">
+      <div className="name">SEVEN UP</div>
+          <div className="soda" >
+            
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
